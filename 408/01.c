f3f8c78c046e055c2aec51f7fca76cb41d53c953
qinguansen <1454538956@qq.com>
2022-10-20 09:15:57 +0800

    add a c

---
+++ 408/01.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define InitSize 100 //表长度的初始定义
+
+typedef bool{
+	false,
+	true
+}bool;
+
+
+typedef struct {
+	ElemType *data;
+	int MaxSize, length;
+}SqList;
+//插入操作
+// *L 指针指向结构体，操作地址后面的元素，一级指针无法改变L本身，需二级指针
+bool ListInsert(SqList *L, int x)
+{
+	
+}
+int main()
+{
+
+	system("chcp 65001");
+
+
+	return 0;
+}
